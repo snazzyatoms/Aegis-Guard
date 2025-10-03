cefe950bb958b15dc6601be2eec1ce6e753b5bd1
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-10-03 15:22:49 -0600

    Update PlayerGUI.java
    Signed-off-by: snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>

--- src/main/java/com/aegisguard/gui/PlayerGUI.java
+++ src/main/java/com/aegisguard/gui/PlayerGUI.java
@@ -4,6 +4,7 @@ import com.aegisguard.AegisGuard;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
@@ -23,38 +24,52 @@ public class PlayerGUI {
         String title = plugin.msg().get("menu_title");
         Inventory inv = Bukkit.createInventory(null, 27, title);
 
-        inv.setItem(11, createItem(
-                Material.LIGHTNING_ROD,
+        inv.setItem(11, createItem(Material.LIGHTNING_ROD,
                 plugin.msg().get("button_claim_land"),
-                plugin.msg().getList("claim_land_lore")
-        ));
+                plugin.msg().getList("claim_land_lore")));
 
-        inv.setItem(13, createItem(
-                Material.PLAYER_HEAD,
+        inv.setItem(13, createItem(Material.PLAYER_HEAD,
                 plugin.msg().get("button_trusted_players"),
-                plugin.msg().getList("trusted_players_lore")
-        ));
+                plugin.msg().getList("trusted_players_lore")));
 
-        inv.setItem(15, createItem(
-                Material.REDSTONE_COMPARATOR,
+        inv.setItem(15, createItem(Material.REDSTONE_COMPARATOR,
                 plugin.msg().get("button_settings"),
-                plugin.msg().getList("settings_lore")
-        ));
+                plugin.msg().getList("settings_lore")));
 
-        inv.setItem(22, createItem(
-                Material.WRITABLE_BOOK,
+        inv.setItem(22, createItem(Material.WRITABLE_BOOK,
                 plugin.msg().get("button_info"),
-                plugin.msg().getList("info_lore")
-        ));
+                plugin.msg().getList("info_lore")));
 
-        inv.setItem(26, createItem(
-                Material.BARRIER,
+        inv.setItem(26, createItem(Material.BARRIER,
                 plugin.msg().get("button_exit"),
-                plugin.msg().getList("exit_lore")
-        ));
+                plugin.msg().getList("exit_lore")));
 
         player.openInventory(inv);
         plugin.sounds().playMenuOpen(player);
+    }
+
+    public void handleClick(Player player, InventoryClickEvent e) {
+        Material type = e.getCurrentItem().getType();
+        switch (type) {
+            case LIGHTNING_ROD -> {
+                player.closeInventory();
+                plugin.selection().confirmClaim(player);
+                plugin.sounds().playMenuFlip(player);
+            }
+            case PLAYER_HEAD -> {
+                plugin.gui().trusted().open(player);
+                plugin.sounds().playMenuFlip(player);
+            }
+            case REDSTONE_COMPARATOR -> plugin.gui().settings().open(player);
+            case WRITABLE_BOOK -> {
+                player.sendMessage(plugin.msg().get("info_message"));
+                plugin.sounds().playMenuFlip(player);
+            }
+            case BARRIER -> {
+                player.closeInventory();
+                plugin.sounds().playMenuClose(player);
+            }
+        }
     }
 
     private ItemStack createItem(Material mat, String name, List<String> lore) {
