7de26afd0c3bfe18849f1c3fc6444df06831e853
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-10-03 15:24:13 -0600

    Update GUIManager.java
    Signed-off-by: snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>

--- src/main/java/com/aegisguard/gui/GUIManager.java
+++ src/main/java/com/aegisguard/gui/GUIManager.java
@@ -1,41 +1,25 @@
 package com.aegisguard.gui;
 
 import com.aegisguard.AegisGuard;
-import org.bukkit.entity.Player;
 
 public class GUIManager {
 
     private final AegisGuard plugin;
     private final PlayerGUI playerGUI;
-    private final TrustedGUI trustedGUI;
     private final SettingsGUI settingsGUI;
     private final AdminGUI adminGUI;
+    private final TrustedGUI trustedGUI;
 
     public GUIManager(AegisGuard plugin) {
         this.plugin = plugin;
         this.playerGUI = new PlayerGUI(plugin);
-        this.trustedGUI = new TrustedGUI(plugin);
         this.settingsGUI = new SettingsGUI(plugin);
         this.adminGUI = new AdminGUI(plugin);
+        this.trustedGUI = new TrustedGUI(plugin);
     }
 
-    public void openPlayerMenu(Player player) {
-        playerGUI.open(player);
-    }
-
-    public void openTrustedMenu(Player player) {
-        trustedGUI.open(player);
-    }
-
-    public void openSettingsMenu(Player player) {
-        settingsGUI.open(player);
-    }
-
-    public void openAdminMenu(Player player) {
-        if (player.hasPermission("aegisguard.admin")) {
-            adminGUI.open(player);
-        } else {
-            plugin.msg().send(player, "no_permission");
-        }
-    }
+    public PlayerGUI player() { return playerGUI; }
+    public SettingsGUI settings() { return settingsGUI; }
+    public AdminGUI admin() { return adminGUI; }
+    public TrustedGUI trusted() { return trustedGUI; }
 }
