2cfa9a875804706465d7b7d17789303990ffb279
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-10-03 15:23:04 -0600

    Update SettingsGUI.java
    Signed-off-by: snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>

--- src/main/java/com/aegisguard/gui/SettingsGUI.java
+++ src/main/java/com/aegisguard/gui/SettingsGUI.java
@@ -4,6 +4,7 @@ import com.aegisguard.AegisGuard;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
@@ -20,81 +21,47 @@ public class SettingsGUI {
     }
 
     public void open(Player player) {
-        String world = player.getWorld().getName();
         Inventory inv = Bukkit.createInventory(null, 54, plugin.msg().get("settings_menu_title"));
 
-        // --- Sounds ---
-        boolean globalEnabled = plugin.getConfig().getBoolean("sounds.global_enabled", true);
-        if (!globalEnabled) {
-            inv.setItem(10, createItem(Material.BARRIER,
-                    plugin.msg().get("button_sounds_disabled_global"),
-                    plugin.msg().getList("sounds_toggle_global_disabled_lore")));
-        } else {
-            boolean soundsEnabled = plugin.isSoundEnabled(player);
-            inv.setItem(10, createItem(
-                    soundsEnabled ? Material.NOTE_BLOCK : Material.BARRIER,
-                    soundsEnabled ? plugin.msg().get("button_sounds_on") : plugin.msg().get("button_sounds_off"),
-                    plugin.msg().getList("sounds_toggle_lore")
-            ));
-        }
+        // Example PvP toggle
+        boolean pvp = plugin.protection().isPvPEnabled(player);
+        inv.setItem(11, createItem(
+                pvp ? Material.IRON_SWORD : Material.WOODEN_SWORD,
+                pvp ? plugin.msg().get("button_pvp_on") : plugin.msg().get("button_pvp_off"),
+                plugin.msg().getList("pvp_toggle_lore")
+        ));
 
-        // --- Per-world protections ---
-        java.util.function.BiPredicate<String, String> worldEnabled = (worldName, key) -> {
-            if (plugin.getConfig().isSet("claims.per_world." + worldName + ".protections." + key)) {
-                return plugin.getConfig().getBoolean("claims.per_world." + worldName + ".protections." + key);
-            }
-            return plugin.getConfig().getBoolean("protections." + key, true);
-        };
+        // ... (repeat for containers, mobs, pets, entities, farms, etc.)
 
-        if (worldEnabled.test(world, "pvp_protection")) {
-            inv.setItem(11, toggleIcon(player, plugin.protection().isPvPEnabled(player),
-                    Material.IRON_SWORD, Material.WOODEN_SWORD,
-                    "button_pvp_on", "button_pvp_off", "pvp_toggle_lore"));
-        }
+        inv.setItem(48, createItem(Material.ARROW,
+                plugin.msg().get("button_back"),
+                plugin.msg().getList("back_lore")));
 
-        if (worldEnabled.test(world, "container_protection")) {
-            inv.setItem(12, toggleIcon(player, plugin.protection().isContainersEnabled(player),
-                    Material.CHEST, Material.TRAPPED_CHEST,
-                    "button_containers_on", "button_containers_off", "container_toggle_lore"));
-        }
-
-        if (worldEnabled.test(world, "mobs_protection")) {
-            inv.setItem(13, toggleIcon(player, plugin.protection().isMobProtectionEnabled(player),
-                    Material.ZOMBIE_HEAD, Material.ROTTEN_FLESH,
-                    "button_mobs_on", "button_mobs_off", "mob_toggle_lore"));
-        }
-
-        if (worldEnabled.test(world, "pets_protection")) {
-            inv.setItem(14, toggleIcon(player, plugin.protection().isPetProtectionEnabled(player),
-                    Material.BONE, Material.LEAD,
-                    "button_pets_on", "button_pets_off", "pet_toggle_lore"));
-        }
-
-        if (worldEnabled.test(world, "entities_protection")) {
-            inv.setItem(15, toggleIcon(player, plugin.protection().isEntityProtectionEnabled(player),
-                    Material.ARMOR_STAND, Material.ITEM_FRAME,
-                    "button_entity_on", "button_entity_off", "entity_toggle_lore"));
-        }
-
-        if (worldEnabled.test(world, "farm_protection")) {
-            inv.setItem(16, toggleIcon(player, plugin.protection().isFarmProtectionEnabled(player),
-                    Material.WHEAT, Material.WHEAT_SEEDS,
-                    "button_farm_on", "button_farm_off", "farm_toggle_lore"));
-        }
-
-        // Navigation
-        inv.setItem(48, createItem(Material.ARROW, plugin.msg().get("button_back"), plugin.msg().getList("back_lore")));
-        inv.setItem(49, createItem(Material.BARRIER, plugin.msg().get("button_exit"), plugin.msg().getList("exit_lore")));
+        inv.setItem(49, createItem(Material.BARRIER,
+                plugin.msg().get("button_exit"),
+                plugin.msg().getList("exit_lore")));
 
         player.openInventory(inv);
         plugin.sounds().playMenuFlip(player);
     }
 
-    private ItemStack toggleIcon(Player player, boolean state, Material onMat, Material offMat,
-                                 String onKey, String offKey, String loreKey) {
-        return createItem(state ? onMat : offMat,
-                state ? plugin.msg().get(onKey) : plugin.msg().get(offKey),
-                plugin.msg().getList(loreKey));
+    public void handleClick(Player player, InventoryClickEvent e) {
+        Material type = e.getCurrentItem().getType();
+        switch (type) {
+            case IRON_SWORD, WOODEN_SWORD -> plugin.protection().togglePvP(player);
+            case CHEST, TRAPPED_CHEST -> plugin.protection().toggleContainers(player);
+            case ZOMBIE_HEAD, ROTTEN_FLESH -> plugin.protection().toggleMobProtection(player);
+            case BONE, LEAD -> plugin.protection().togglePetProtection(player);
+            case ARMOR_STAND, ITEM_FRAME -> plugin.protection().toggleEntityProtection(player);
+            case WHEAT, WHEAT_SEEDS -> plugin.protection().toggleFarmProtection(player);
+
+            case ARROW -> plugin.gui().player().open(player);
+            case BARRIER -> {
+                player.closeInventory();
+                plugin.sounds().playMenuClose(player);
+            }
+        }
+        open(player); // refresh after toggle
     }
 
     private ItemStack createItem(Material mat, String name, List<String> lore) {
